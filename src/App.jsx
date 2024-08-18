3a60f46a86669124c7418970f128278d74a91670
haxy <zaidsaheb4@gmail.com>
2024-08-18 23:23:48 +0300

    optimized website performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Intro from "./components/intro";
 import AboutMe from "./components/aboutme";
 import Experience from "./components/experience";
 import Projects from "./components/projects";
 import NavBar from "./components/navigation";
 import "./styles/app.css";
+import smoothscroll from 'smoothscroll-polyfill';
 
-export default function App() { //Always add ambient light to the scene
+export default function App() {
+
+  useEffect(() => {
+    smoothscroll.polyfill();
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
