159f642f2fb63607f3cfef332edf1b6435893a8e
haxy <zaidsaheb4@gmail.com>
2024-08-19 00:47:29 +0300

    started working vertical tabs finally

---
+++ src/components/jobData.jsx
@@ -0,0 +1,38 @@
+export const jobData = [
+  {
+    value: 'tab1',
+    label: 'Tab 1',
+    content: [
+      'First point for Tab 1',
+      'Second point for Tab 1',
+      'Third point for Tab 2',
+    ],
+  },
+  {
+    value: 'tab2',
+    label: 'Tab 2',
+    content: [
+      'First point for Tab 2',
+      'Second point for Tab 2',
+      'Third point for Tab 2',
+    ],
+  },
+  {
+    value: 'tab3',
+    label: 'Tab 3',
+    content: [
+      'First point for Tab 3',
+      'Second point for Tab 3',
+      'Third point for Tab 3',
+    ],
+  },
+  {
+    value: 'tab4',
+    label: 'Tab 4',
+    content: [
+      'First point for Tab 4',
+      'Second point for Tab 4',
+      'Third point for Tab 4',
+    ],
+  },
+];
--- src/components/verticaltabs.jsx
+++ src/components/verticaltabs.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import * as Tabs from '@radix-ui/react-tabs';
 import '../styles/verticalTabs.css';
+import {jobData} from './jobData';
 
 export default function VerticalTabs() {
   return (
-    <Tabs.Root defaultValue="tab1" className="vertical-tabs-container">
+    <Tabs.Root defaultValue={jobData[0].value} className="vertical-tabs-container">
       <Tabs.List className="vertical-tabs-list" orientation="vertical">
-        <Tabs.Trigger value="tab1" className="vertical-tab-trigger">
-          Tab 1
-        </Tabs.Trigger>
-        <Tabs.Trigger value="tab2" className="vertical-tab-trigger">
-          Tab 2
-        </Tabs.Trigger>
-        <Tabs.Trigger value="tab3" className="vertical-tab-trigger">
-          Tab 3
-        </Tabs.Trigger>
+        {jobData.map((tab) => (
+          <Tabs.Trigger key={tab.value} value={tab.value} className="vertical-tab-trigger">
+            {tab.label}
+          </Tabs.Trigger>
+        ))}
       </Tabs.List>
 
-      <Tabs.Content value="tab1" className="vertical-tab-content">
-        <p>Content for Tab 1</p>
-      </Tabs.Content>
-      <Tabs.Content value="tab2" className="vertical-tab-content">
-        <p>Content for Tab 2</p>
-      </Tabs.Content>
-      <Tabs.Content value="tab3" className="vertical-tab-content">
-        <p>Content for Tab 3</p>
-      </Tabs.Content>
+      {jobData.map((tab) => (
+        <Tabs.Content key={tab.value} value={tab.value} className="vertical-tab-content">
+         <ul>
+            {tab.content.map((point, index) => (
+              <li key={index}>{point}</li>
+            ))}
+          </ul>
+        </Tabs.Content>
+      ))}
     </Tabs.Root>
   );
 }
