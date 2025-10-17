e6912c3224209da0a34f610f145f9adbb75c1496
haxybaxy <zaidsaheb4@gmail.com>
2025-10-17 17:48:59 +0200

    close sound effect

--- src/components/window.jsx
+++ src/components/window.jsx
@@ -1,44 +1,55 @@
 import { useState } from "react";
+import Draggable from 'react-draggable'
 import "../styles/window.css";
 import FadeInSection from "./fadeinsection";
 import { Typewriter } from 'react-simple-typewriter';
+import useSound from 'use-sound';
+
 
 export default function Window({ children, title, id, filename, headerstyle, onClose }) {
   const [startTyping, setStartTyping] = useState(false);
+  const [playClick] = useSound('/sounds/toc-click.wav', { volume: 0.5 });
   return (
     <FadeInSection onVisible={() => setStartTyping(true)}>
-      <div className="window" id={id}>
-        <div className="windowHeader">
-          <ul className="windowControls">
-            <li className="closeWindow" onClick={onClose} style={{ cursor: 'pointer' }}>X</li>
-          </ul>
-          <div className="filename">
-            <img src="folder.svg" alt="folder" className="folderIcon" />
-            <span>{filename}</span>
+      <Draggable>
+        <div className="window" id={id}>
+          <div className="windowHeader">
+            <ul className="windowControls">
+              <li className="closeWindow"
+                onClick={() => {
+                  onClose()
+                  playClick();
+                }}
+                style={{ cursor: 'pointer' }}>X</li>
+            </ul>
+            <div className="filename">
+              <img src="folder.svg" alt="folder" className="folderIcon" />
+              <span>{filename}</span>
+            </div>
           </div>
-        </div>
-        <div className="sectionHeader" style={headerstyle}>
-          <h1 className="promptchars"> <span style={{ color: "#74c7ec" }}> ~ </span> ❯</h1>
-          <span className="typeAnimation">
-            {startTyping && <Typewriter
-              words={[title]}
-              loop={1}
-              cursor={false}
-              cursorStyle='|'
-              cursorBlinking={false}
-              typeSpeed={70}
-              delaySpeed={1000}
-            />}
-          </span>
-          <hr className="headerLine"></hr>
-        </div>
-        <div className="contentContainer">
-          <FadeInSection delay={'900ms'}>
-            {children}
-          </FadeInSection>
-        </div>
+          <div className="sectionHeader" style={headerstyle}>
+            <h1 className="promptchars"> <span style={{ color: "#74c7ec" }}> ~ </span> ❯</h1>
+            <span className="typeAnimation">
+              {startTyping && <Typewriter
+                words={[title]}
+                loop={1}
+                cursor={false}
+                cursorStyle='|'
+                cursorBlinking={false}
+                typeSpeed={70}
+                delaySpeed={1000}
+              />}
+            </span>
+            <hr className="headerLine"></hr>
+          </div>
+          <div className="contentContainer">
+            <FadeInSection delay={'900ms'}>
+              {children}
+            </FadeInSection>
+          </div>
 
-      </div>
+        </div>
+      </Draggable>
     </FadeInSection>
   );
 }
